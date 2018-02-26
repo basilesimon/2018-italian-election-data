b282f4018569a6f8ce50b5f1ca9a3a010755bcfd
basilesimon <basile@basilesimon.fr>
2018-02-26 19:14:16 +0000

    Regression models

--- 01_GatherData.R
+++ 01_GatherData.R
@@ -1,7 +1,9 @@
 library(readr)
 library(dplyr)
+library(broom)
 library(ggplot2)
 library(reshape2)
+library(gridExtra)
 
 # opposite of intersect()
 outersect <- function(x, y) {
@@ -9,6 +11,8 @@ outersect <- function(x, y) {
          setdiff(y, x)))
 }
 
+#########################################################################
+# load all data
 provinces_regions <- read_csv("data/provinces_regions.csv")
 results_provinces_2013 <- read_csv("data/2013_results_provinces.csv")
 results_districts_2013 <- read_csv("data/2013_results_districts.csv")
@@ -20,7 +24,7 @@ poverty <- read_csv("data/ilc_peps11_1_Data.csv") %>%
   select(-`Flag and Footnotes`, -TIME, -UNIT)
 names(poverty) <- c('District', 'poverty_2016')
 
-merge(results_districts_2013, poverty, by='District') %>%
+poverty_plot <- merge(results_districts_2013, poverty, by='District') %>%
   select(-District) %>%
   melt(id=c("District Long", "poverty_2016")) %>% 
   na.omit() %>%
@@ -31,3 +35,46 @@ merge(results_districts_2013, poverty, by='District') %>%
   facet_grid(. ~ variable) + 
   geom_smooth(method="lm") +
   ggtitle(label=poverty_title)
+
+# usual resident population by NUTS-3
+# source: Eurostat | data: 2016
+age_title <- "Over 65 vote, by party"
+age_groups <- read_csv("data/20152016_age_groups.csv")
+names(age_groups) <- c('Province', '15_64', 'over_65')
+
+age_plot <- merge(results_provinces_2013, age_groups) %>% 
+  mutate(ratio = `over_65` / `15_64`) %>% 
+  select(-`15_64`, -`over_65`) %>%
+  melt(id=c("Province", "ratio")) %>%
+  mutate(vote_share = as.numeric(value)) %>% 
+  ggplot(aes(x=ratio, y=vote_share, color=variable)) + geom_point(aes(alpha=0.5)) +
+  geom_smooth(method="lm") +
+  facet_grid( . ~ variable) +
+  ggtitle(label=age_title)
+
+################################################################################
+# arrange plots together
+grid.arrange(poverty_plot, age_plot, nrow=2)
+
+################################################################################
+# look at regressions
+poverty_groups <- merge(results_districts_2013, poverty, by='District') %>%
+  select(-District) %>%
+  melt(id=c("District Long", "poverty_2016")) %>% 
+  na.omit() %>%
+  mutate(vote_share = as.numeric(value)) %>%
+  select(-value) %>% 
+  group_by(variable)
+poverty_models <- poverty_groups %>% group_by(variable) %>%
+  do(model = lm(vote_share ~ poverty_2016, data=.))
+poverty_models %>% glance(model)
+
+age_groups <- merge(results_provinces_2013, age_groups) %>% 
+       mutate(ratio = `over_65` / `15_64`) %>% 
+       select(-`15_64`, -`over_65`) %>%
+       melt(id=c("Province", "ratio")) %>%
+       mutate(vote_share = as.numeric(value)) %>% 
+       group_by(variable)
+age_models <- age_groups %>% group_by(variable) %>%
+       do(model = lm(vote_share ~ ratio, data=.))
+age_models %>% glance(model)
